2b93c0c741d9991d7ad2fd38ede95328d0a30b11
ISE <ise.sns46@gmail.com>
2025-10-01 16:23:02 +0700

    Make every view scrollable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,14 +66,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
-    // You have a duplicate recyclerview dependency, you can remove one
-    // implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation(libs.androidx.cardview) // Use the version catalog alias
+    implementation(libs.androidx.cardview)
     implementation(libs.google.flexbox)
 
     testImplementation(libs.junit)
